82d14647e88517af26d15e68d0bb857ad89432e7
Jordan <jordanwood3119@gmail.com>
2025-10-22 17:02:12 -0700

    Update TransactionRepository.java

--- BankingApp/BankingApp/src/main/java/com/Jordan/BankingApp/repository/TransactionRepository.java
+++ BankingApp/BankingApp/src/main/java/com/Jordan/BankingApp/repository/TransactionRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    // Fetch distinct transactions by account ID to avoid duplicates
     List<Transaction> findDistinctByAccount_Id(Long accountId);
+
 }
